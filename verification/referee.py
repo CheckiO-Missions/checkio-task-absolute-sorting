66b392645a6ca6c49dbb5f68e8cbd4feaf1301f2
Alexander Blaschuk <blaschuk95@gmail.com>
2022-08-23 16:17:58 +0300

    update

--- verification/referee.py
+++ verification/referee.py
@@ -1,8 +1,7 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
-from checkio.referees.io import CheckiOReferee
-from checkio.referees import cover_codes
-from checkio.referees import checkers
+from checkio.referees.io_template import CheckiOReferee
+# from checkio.referees.checkers import to_list
 
 from tests import TESTS
 
@@ -10,8 +9,15 @@ api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
+        # checker=to_list,
         function_name={
-            "python": "checkio",
+            "python":"checkio",
             "js": "absoluteSorting"
+        },
+        cover_code={
+            'python-3': {},
+            'js-node': {
+                # "dateForZeros": True,
+            }
         }
     ).on_ready)
